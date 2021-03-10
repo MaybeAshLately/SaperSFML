eec8c9556cdd970e842b07d981f9d46a5aabb5fe
MaciejGuzewski <259656@student.pwr.edu.pl>
2021-03-10 13:46:16 +0000

    Rozpoczęcie pracy; utworzenie konstruktora; zdefiniowanie wyświetlania planszy

--- main.cpp
+++ main.cpp
@@ -1,6 +1,78 @@
 #include <iostream>
 
+using namespace std;
+
+void intro()
+{
+  cout<<endl;
+  cout<<"Maciej (----56)"<<endl;
+  cout<<endl;
+}
+
+struct Field
+{
+	bool hasMine;
+	bool hasFlag;
+	bool isRevealed;
+};
+
+class MinesweeperBoard
+{
+  Field board[100][100];    // 100-elementowa tablica, 100-elementowych tablic typu Field 
+  int width;                // rzeczywista szerokość planszy
+  int height;               // rzeczywista wysokość planszy
+
+public:
+  MinesweeperBoard();
+  void debug_display() const;
+};
+
+MinesweeperBoard::MinesweeperBoard()
+{
+  width=5;
+  height=5;
+
+  for(int wie=0;wie<=width-1;wie++)
+  {
+    for(int kol=0;kol<=height-1;kol++)
+    {
+      board[wie][kol].hasMine=0;
+      board[wie][kol].hasFlag=0;
+      board[wie][kol].isRevealed=0;
+    }
+    
+  }
+  
+}
+
+void MinesweeperBoard::debug_display() const
+{
+  for(int wie=0;wie<=width-1;wie++)
+  {
+    for(int kol=0;kol<=height-1;kol++)
+    {
+      cout<<"[";
+      if(board[wie][kol].hasMine==true) cout<<"M";
+      else cout<<".";
+      if(board[wie][kol].isRevealed==true) cout<<"o";
+      else cout<<".";
+      if(board[wie][kol].hasFlag==true ) cout<<"f";
+      else cout<<".";
+      cout<<"] ";
+    }
+    cout<<endl;
+    
+  }
+}
 int main() {
-  std::cout << "Hello World!\n";
-  //zmiana
+  intro();
+
+  MinesweeperBoard test;
+  
+  cout<<endl;
+  cout<<endl; 
+
+  test.debug_display();
+  
+
 }
